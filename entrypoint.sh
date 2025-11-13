2ae84edbc46e72417ec793cefbeddef2adaae0b8
kevinruffus <kevinruffus@gmail.com>
2025-11-13 10:13:17 -0600

    Testing security conf updates and memory of sed commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,14 @@ chmod -R 770 /etc/zm /var/log/zm
 echo "Setting PHP timezone"
 sed -i "s|;date\.timezone =.*|date.timezone = ${TZ}|" /etc/php/8.4/apache2/php.ini
 
+# Configures security options in security.conf
+echo "Configuring Apache security settings"
+sed -i "s/ServerTokens OS/ServerTokens Prod/" /etc/apache2/conf-available/security.conf
+sed -i "s/ServerSignature On/ServerSignature Off/" /etc/apache2/conf-available/security.conf
+sed -i '/#Header set X-Content-Type-Options: "nosniff"/Header set X-Content-Type-Options: "nosniff"' /etc/apache2/conf-available/security.conf
+sed -i '/#Header set Content-Security-Policy "frame-ancestors 'self';"/ a Header set X-Frame-Options: "sameorigin"' /etc/apache2/conf-available/security.conf
+sed -i '/Header set X-Frame-Options: "sameorigin"/ a Header always unset X-Powered-By' /etc/apache2/conf-available/security.conf
+
 echo "Setting up directories in /run tmpfs"
 install -m 0755 -o root -g root -d /run/apache2
 install -m 1777 -o root -g root -d /run/lock
